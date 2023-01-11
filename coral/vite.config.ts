7032bdfd3629e796df4ae33c2a8e03e0c018d2b2
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-01-11 14:37:52 +0100

    Add feature flag for requesting schema for topic.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- coral/vite.config.ts
+++ coral/vite.config.ts
@@ -137,6 +137,9 @@ export default defineConfig(({ mode }) => {
         FEATURE_FLAG_TOPIC_REQUEST: ["development", "remote-api"]
           .includes(mode)
           .toString(),
+        FEATURE_FLAG_TOPIC_SCHEMA_REQUEST: ["development", "remote-api"]
+          .includes(mode)
+          .toString(),
       },
     },
     css: {
